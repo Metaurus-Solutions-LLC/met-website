9b1052e516c72acce4b3e3a6618b921ff1f37601
Ted Wetekamp <ted.wetekamp@yahoo.com>
2024-07-19 11:15:12 -0500

    vite.config.js back to base: /met-website/.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
 //  plugins: [react()],
-  base: "/", plugins: [react()],
+  base: "/met-website/", plugins: [react()],
 })
